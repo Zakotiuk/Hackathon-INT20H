545b4abefb09d7b685adf915e17ab2f5a130aa0b
Zakotiuk <vzakotuk@gmail.com>
2023-02-07 23:41:50 +0200

    Changes in category

--- src/components/category_item/category_item.jsx
+++ src/components/category_item/category_item.jsx
@@ -1,20 +1,25 @@
 import React, { useRef } from "react";
+import { useEffect } from "react";
 import './category_item.css';
 
-let lastActive = 0;
+let lastActive = [];
 let lastItem = {"blackIcon" : "https://img.icons8.com/ios-glyphs/500/null/steak-medium.png"};
-const Category_item = ({item, index, setCategoryQuery}) =>{
+const Category_item = ({item, refff, index, setCategoryQuery}) =>{
     const btn = useRef(null);
     const changeActive = () =>{
-        document.getElementById(lastActive).classList.replace("active", "non_active");
-        document.getElementById(lastActive).childNodes[0].src = lastItem.blackIcon;
+        lastActive.classList?.replace("active", "non_active");
+        lastActive.childNodes[0].src = lastItem.blackIcon;
 
         btn.current.classList.replace("non_active", "active");
         btn.current.childNodes[0].src = item.whiteIcon;
         
-        lastActive = btn.current.id;
+        lastActive = btn.current;
         lastItem = item;
     }
+
+    useEffect(()=>{
+        lastActive = refff.current.childNodes[0];
+    },[]);
 
     return(
         <div ref={btn} id={index} onClick={()=> {setCategoryQuery(item.name); changeActive()}} className={index == 0 ? "category_item_div active" : "category_item_div non_active"}>
--- src/components/category_list/category_list.jsx
+++ src/components/category_list/category_list.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from "react";
+import { useRef } from "react";
 import Category_item from "../category_item/category_item";
 import './category_list.css'
 const Category_list = ({ categories,setCategoryQuery, title}) =>{
     const setUserCategoryQuery = (category) =>{
         setCategoryQuery(category);
     }
+    const myReff = useRef(null)
     return(
         <div id="categories" className="category_list_full_div">
             {
                 title &&
                 <span className="fnt_fml_bbs fnt_sz_36 logo">{title}</span>
             }
-            <div className="category_list_div">
+            <div ref={myReff} className="category_list_div">
                 {
                     categories.map((item, index) =>
-                        <Category_item index={index} setCategoryQuery={setUserCategoryQuery} item={item} key={index}></Category_item>
+                        <Category_item refff={myReff} index={index} setCategoryQuery={setUserCategoryQuery} item={item} key={index}></Category_item>
                     )
                 }
             </div>
